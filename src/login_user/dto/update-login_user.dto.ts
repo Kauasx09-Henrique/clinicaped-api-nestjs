21aa0ab0175facdd3bf117de63a9db019f8b1021
Kauã Henrique <kauaheenriquesx09@gmail.com>
2025-06-05 13:35:03 -0300

    fi

--- src/login_user/dto/update-login_user.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { LoginUserDto } from './create-login_user.dto';
-
-export class UpdateLoginUserDto extends PartialType(LoginUserDto) {}
--- src/login_user/login_user.service.ts
+++
@@ -1,4 +0,0 @@
-import { Injectable } from '@nestjs/common';
-// Importa o repositório do usuário, por exemplo, para consultar no banco
-import { UsersService } from '../users/users.service';
-
